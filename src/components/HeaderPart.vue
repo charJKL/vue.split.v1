56818e0b178a543a98cc3d3b8c62ea94ec00825c
charJKL <11600464+charJKL@users.noreply.github.com>
2021-08-30 20:37:40 +0200

    End selectable list.

---
+++ src/components/HeaderPart.vue
@@ -0,0 +1,33 @@
+<template>
+<header>
+	<div>
+		<input type="file" multiple @change="onChange"/>
+	</div>
+</header>
+</template>
+
+<script>
+export default
+{
+	methods: 
+	{
+		onChange(e)
+		{
+			console.log('emit?');
+			this.$emit('files', e.target.files);
+		}
+	}
+}
+</script>
+
+<style>
+header
+{
+	flex: 0 0 content;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-between;
+	padding: 10px;
+	background: var(--gray);
+}
+</style>
---
+++ src/components/ListPart.vue
@@ -0,0 +1,60 @@
+<template>
+<section>
+	<img v-for="(image) in list" :src="image.src" :key="image.name" :class="isCurrent(image.name)" @click="selectCurrent(image.name)"/>
+</section>
+</template>
+
+<script>
+export default
+{
+	props:
+	{
+		list: { type: Array, requred: true },
+	},
+	data()
+	{
+		return {
+			current: null,
+		}
+	},
+	methods:
+	{
+		isCurrent(name)
+		{
+			return this.current === name ? 'current' : '';
+		},
+		selectCurrent(name)
+		{
+			this.current = name;
+			this.$emit('current', name);
+		}
+	}
+}
+</script>
+
+<style>
+section
+{
+	flex: 0 0 content;
+	display: flex;
+	flex-flow: row nowrap;
+	overflow-y: auto;
+	padding: 10px;
+	background: var(--gray-light);
+}
+section > img
+{
+	height: 100px;
+	margin: 0px 5px;
+	border: solid 2px #000;
+}
+section > img:hover
+{
+	border: solid 2px #00f;
+	cursor:pointer;
+}
+section > img.current
+{
+	border: solid 2px #f00;
+}
+</style>
