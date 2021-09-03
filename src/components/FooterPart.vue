7cc9be6e3fda6aef65435416c9834610286138e6
charJKL <11600464+charJKL@users.noreply.github.com>
2021-09-03 08:51:56 +0200

    Refactor naming.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -4,11 +4,11 @@
 		<template v-if="current">
 		[
 			<input :value="current.name" />,
-			<template  v-for="(metric, key, index) in current.metrics" :key="index">
-				{{ key }}:
-				<input v-if="metric.type === 'value'" class="input" step="0.1" type="number" v-model="metric.value" @input="onInputFloatEvent(key, $event)" />
-				<input v-else-if="metric.type === 'manual'" class="input" type="checkbox" v-model="metric.value" @checked="onInputEvent(key, $event)"/>
-				<input v-else class="input" type="number" v-model="metric.value" @input="onInputIntEvent(key, $event)" />,
+			<template  v-for="(metric, name, index) in current.metrics" :key="index">
+				{{ name }}:
+				<input v-if="metric.type === 'value'" class="input" step="0.1" type="number" v-model="metric.value" @input="onInputFloatEvent(name, $event)" />
+				<input v-else-if="metric.type === 'manual'" class="input" type="checkbox" v-model="metric.value" @checked="onInputEvent(name, $event)"/>
+				<input v-else class="input" type="number" v-model="metric.value" @input="onInputIntEvent(name, $event)" />,
 			</template>
 		]
 		</template>
@@ -31,18 +31,18 @@ export default
 	},
 	methods:
 	{
-		onInputIntEvent(key, e)
+		onInputIntEvent(name, e)
 		{
-			this.onInputEvent(key, parseInt(e.target.value));
+			this.onInputEvent(name, parseInt(e.target.value));
 		},
-		onInputFloatEvent(key, e)
+		onInputFloatEvent(name, e)
 		{
-			this.onInputEvent(key, parseFloat(e.target.value));
+			this.onInputEvent(name, parseFloat(e.target.value));
 		},
-		onInputEvent(key, value)
+		onInputEvent(name, value)
 		{
 			const current = _.cloneDeep(this.current);
-					current.metrics[key].value = value;
+					current.metrics[name].value = value;
 			this.$emit('update:current', current);
 		}
 	}
