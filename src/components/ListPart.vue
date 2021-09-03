ce970284fa7cf41601b06def55785fe5774088cc
charJKL <11600464+charJKL@users.noreply.github.com>
2021-09-03 17:52:18 +0200

    Change styles in list.

--- src/components/ListPart.vue
+++ src/components/ListPart.vue
@@ -1,10 +1,11 @@
 <template>
-<section class="list">
+<section ref="list" class="list" @wheel.prevent="onMouseWheel">
 	<div v-for="(image, index) in list" :key="image.name" class="thumbnail" :class="isCurrent(image.name)" @click="selectCurrent(image.name, index)">
-		<img :src="image.src" />
-		<div v-if="image.wasEdited" class="check">✓</div>
-		<div v-if="image.metrics.layout.value" class="marked">◯</div>
-		<div class="name">{{ image.name }}</div> 
+		<img class="thumbnail-image" :src="image.src" />
+		<div class="thumbnail-overflow"></div>
+		<div class="thumbnail-marked" v-if="image.metrics.layout.value" >◯</div>
+		<div class="thumbnail-check" v-if="image.wasEdited" >✓</div>
+		<div class="thumbnail-name">{{ image.name }}</div> 
 	</div>
 </section>
 </template>
@@ -32,6 +33,10 @@ export default
 		{
 			this.current = name;
 			this.$emit('current', index);
+		},
+		onMouseWheel(e)
+		{
+			this.$refs.list.scrollLeft += e.deltaY;
 		}
 	}
 }
@@ -51,53 +56,55 @@ export default
 .thumbnail
 {
 	position: relative;
-	box-sizing: border-box;
-	height: 100px;
+	height: 96px;
 	margin: 0px 5px;
+	cursor: pointer;
 	border: solid 2px #000;
 }
-.thumbnail:hover
-{
-	border: solid 2px #00f;
-	cursor:pointer;
-}
-.thumbnail.current
-{
-	border: solid 2px #f00;
-}
-.thumbnail > img
+.thumbnail-image
 {
 	height: 100%;
 	object-fit: cover;
-}
-.check
-{
-	position: absolute;
-	width: 100%;
-	left: 0px; top: 25px;
-	text-align: center;
-	font: 50px / 50px var(--font);
-	color: #0d0;
-	z-index: 2;
-}
-.marked
-{
-	position: absolute;
-	width: 100%;
-	left: 0px; top: 25px;
-	text-align: center;
-	font: 40px / 40px var(--font);
-	color: #f00;
 	z-index: 1;
 }
-.name
+.thumbnail-overflow
+{
+	position: absolute;
+	top:0px; left: 0px;
+	width: 100%; height: 100%;
+	z-index: 2;
+}
+.thumbnail:hover .thumbnail-overflow{ background: rgba(0, 0, 255, .3); }
+.thumbnail.current .thumbnail-overflow{ background: rgba(255, 0, 0, .3); }
+.thumbnail-marked
+{
+	position: absolute;
+	width: 100%; height: 100%;
+	left: 0px; top: 0px;
+	text-align: center;
+	font: bold 35px / 100px var(--font);
+	color: #f00;
+	z-index: 3;
+}
+.thumbnail-check
+{
+	position: absolute;
+	width: 100%; height: 100%;
+	left: 0px; top: 0px;
+	text-align: center;
+	font: 50px / 100px var(--font);
+	color: #0d0;
+	z-index: 4;
+}
+.thumbnail-name
 {
 	position: absolute;
 	width: 100%;
 	left: 0px; bottom: 0px;
 	text-align: center;
 	font: 11px / 20px var(--font);
-	background: rgba(0, 0, 0, .5);
+	background: rgba(0, 0, 0, .6);
 	color: #fff;
+	z-index: 5;
 }
 </style>
