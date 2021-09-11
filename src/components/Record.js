e36901ab403a05e7cdffbfa2257baa15d54e65a2
charJKL <11600464+charJKL@users.noreply.github.com>
2021-09-11 10:42:13 +0200

    Add Record blueprint file.

---
+++ src/components/Record.js
@@ -0,0 +1,14 @@
+export default
+{
+	name: '',
+	wasEdited: false,
+	metrics:
+	{
+		x1: { type: 'line', subtype: 'vertical', value: 0 },
+		x2: { type: 'line', subtype: 'vertical', value: 0 },
+		y1: { type: 'line', subtype: 'horizontal', value: 0 },
+		y2: { type: 'line', subtype: 'horizontal', value: 0 },
+		rotate: { type: 'value', value: 0},
+		layout: { type: 'manual', value: false },
+	}
+}
