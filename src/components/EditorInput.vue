e2106a50a71ea2ba0d10f4688db47a51fa612af3
charJKL <11600464+charJKL@users.noreply.github.com>
2021-09-30 11:52:19 +0200

    Refactor EditorInput.vue

--- src/components/EditorInput.vue
+++ src/components/EditorInput.vue
@@ -1,9 +1,10 @@
 <template>[
-	name: <input class="input-text" ref="name" :value="name" @input="onInputNameEvent" />
-	<template v-for="metric in metrics" :key="metric.name">
+	name: <input class="input-text" v-model="internal.filename" @update:modelValue="updateSource" />,
+	<template v-for="metric in local" :key="metric.name">
 		<label>
 			{{ metric.name }}:
-			<input :value="metric.value" />
+			<input class="input-int" v-if="isLine(metric.type)" type="number" :disabled="metric.isDisabled" v-model.number="metric.value" @update:modelValue="updateMetrics" />
+			<input class="input-float" v-if="isFloat(metric.type)" type="number" step="0.1" :disabled="metric.isDisabled" v-model.number="metric.value" @update:modelValue="updateMetrics" />
 		</label>,
 	</template>]
 </template>
@@ -13,23 +14,67 @@ import Record from './Record';
 import {isMatch} from '../core/isMatch';
 import {cloneDeep} from 'lodash';
 
+const updateSource = 'update:source';
+const updateMetrics = 'update:metrics';
+
 export default
 {
 	props:
 	{
-		name: { type: String },
-		metrics: { type: Object, required: true, validator(value){ return isMatch(Record.metrics, value); } },
+		source: { type: Object, validator(value){ return isMatch(Record.source, value); } },
+		metrics: { type: Object, validator(value){ return isMatch(Record.metrics, value); } },
 	},
-	emits: ['update:name', 'update:metrics'],
+	emits: [updateSource, updateMetrics],
 	computed:
 	{
-		metric()
+		isSourceNull()
 		{
-			return this.metric.filter();
+			return this.source === null;
+		},
+		isMetricsNull()
+		{
+			return this.metrics === null;
+		},
+		internal()
+		{
+			return cloneDeep(this.isSourceNull ? Record.source : this.source);
+		},
+		local()
+		{
+			return this.wrapMetrics(cloneDeep(this.isMetricsNull ? Record.metrics : this.metrics));
 		}
+	},
+	watch:
+	{
+		
 	},
 	methods:
 	{
+		updateSource()
+		{
+			const source = cloneDeep(this.source);
+			this.$emit(updateSource, source);
+		},
+		updateMetrics()
+		{
+			const metrics = cloneDeep(this.metrics);
+					metrics.x1.value = this.local.x1.value;
+					metrics.x2.value = this.local.x2.value;
+					metrics.y1.value = this.local.y1.value;
+					metrics.y2.value = this.local.y2.value;
+					metrics.rotate.value = this.local.rotate.value;
+			this.$emit(updateMetrics, metrics);
+		},
+		wrapMetrics(metrics)
+		{
+			metrics.x1.isDisabled = this.isSourceNull;
+			metrics.x2.isDisabled = this.isSourceNull;
+			metrics.y1.isDisabled = this.isSourceNull;
+			metrics.y2.isDisabled = this.isSourceNull;
+			metrics.rotate.isDisabled = this.isSourceNull;
+			
+			return metrics;
+		},
 		isLine(type)
 		{
 			return type === 'line';
@@ -38,32 +83,17 @@ export default
 		{
 			return type === 'float';
 		},
-		getValue(name)
+		onInputTextEvent(name, e)
 		{
-			this.metrics[name].value;
+			this.updateSource(name, e.target.value);
 		},
-		onInputIntEvent(name, e)
-		{
-			this.onInputEvent(name, parseInt(e.target.value));
-		},
-		onInputFloatEvent(name, e)
-		{
-			this.onInputEvent(name, parseFloat(e.target.value));
-		},
-		onInputNameEvent(value)
-		{
-			this.$emit('update:name', value);
-		},
-		onInputEvent(name, value)
-		{
-			const metrics = cloneDeep(this.metrics);
-					metrics[name].value = value;
-			this.$emit('update:metrics', metrics);
-		}
 	}
 }
 </script>
 
 <style scoped>
-
+input
+{
+	margin: 0px 5px 0px 0px;
+}
 </style>
