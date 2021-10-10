24d486cb4bde4d7a1a2ee9ddd04c52f293095de3
charJKL <11600464+charJKL@users.noreply.github.com>
2021-10-10 18:26:44 +0200

    Update isMatch function, check properties length.

--- src/lib/isMatch.js
+++ src/lib/isMatch.js
@@ -1,10 +1,12 @@
 function isMatch(blueprint, object)
 {
+	if(Object.keys(blueprint).length !== Object.keys(object).length) return false;
+	
 	for(let [key, value] of Object.entries(blueprint))
-		{
-			if(object[key] === undefined) return false;
-			if(typeof object[key] !== typeof value) return false;
-		}
+	{
+		if(object[key] === undefined) return false;
+		if(typeof object[key] !== typeof value) return false;
+	}
 	return true;
 }
 
