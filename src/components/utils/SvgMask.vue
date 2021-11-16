4ccc02a213ef51752fbc112699915097d6bed097
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-16 12:30:10 +0100

    Implement `SvgMask` component.

---
+++ src/components/utils/SvgMask.vue
@@ -0,0 +1,59 @@
+<template>
+	<mask id="views">
+		<rect class="view-shadow" />
+		<rect class="view-spot" :style="getSpotStyle" />
+	</mask>
+	<rect class="size" mask="url(#views)" :style="getSizeStyle" />
+</template>
+
+<script>
+import {isMatch} from '../../lib/isMatch';
+
+const blueprint = 
+{
+	rect: {x: 0, y:0, width: 0, height: 0},
+}
+
+export default
+{
+	props:
+	{
+		size: { type: Object, required: true, validator(value){ return isMatch(blueprint.rect, value); } },
+		spot: { type: Object, required: true, validator(value){ return isMatch(blueprint.rect, value); } },
+	},
+	computed:
+	{
+		getSizeStyle()
+		{
+			return {x: this.size.x, y: this.size.y, width: this.size.width, height: this.size.height };
+		},
+		getSpotStyle()
+		{
+			return {x: this.spot.x, y: this.spot.y, width: this.spot.width, height: this.spot.height };
+		},
+	}
+}
+</script>
+
+<style scoped>
+.size
+{
+	x: 0;
+	y: 0;
+	width: 100%;
+	height: 100%;
+	fill: rgba(0, 0, 0, .3);
+}
+.view-shadow
+{
+	fill: #fff;
+	x: 0;
+	y: 0;
+	width: 100%;
+	height: 100%;
+}
+.view-spot
+{
+	fill: #000;
+}
+</style>
