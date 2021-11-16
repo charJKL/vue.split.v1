265f85138dcf68117cf78e429f54a327b38f4ade
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-16 19:28:54 +0100

    Add current selection idicatior for `EditorThumbnail`.

--- src/components/EditorThumbnail.vue
+++ src/components/EditorThumbnail.vue
@@ -1,9 +1,17 @@
 <template>
-	<div class="image" ref="thumbnail">
-		<div class="name">{{ getName }}</div>
-		<canvas-mask class="mask" :size="imageSize" :spot="spotPosition"></canvas-mask>
-		<img class="img" :src="getImageUrl" />
-	</div>
+	<template v-if="current.loaded == true">
+		<div :class="['editor', getEditorClasses]" v-bind="$attrs" ref="thumbnail">
+			<div class="name">{{ getName }}</div>
+			<canvas-mask class="mask" :size="imageSize" :spot="spotPosition"></canvas-mask>
+			<img class="img" :style="getImageStyle" :src="getImageUrl" />
+		</div>
+	</template>
+	<template v-if="current.loaded == false">
+		<div :class="['editor', getEditorClasses]" v-bind="$attrs">
+			<img class="waiting" src="../assets/waiting.svg" />
+			<div class="name">{{ getName }}</div>
+		</div>
+	</template>
 </template>
 
 <script>
@@ -22,9 +30,18 @@ export default
 	},
 	computed:
 	{
+		getEditorClasses()
+		{
+			const isSelected = this.$store.getters.source == this.source ? 'selected' : '';
+			return [isSelected];
+		},
 		getImageUrl()
 		{
 			return this.current.url;
+		},
+		getImageStyle()
+		{
+			return { transform: `rotate(${this.scaled.rotate.value}deg)` };
 		},
 		getName()
 		{
@@ -40,7 +57,6 @@ export default
 		{
 			const width = this.scaled.x2.value - this.scaled.x1.value;
 			const height = this.scaled.y2.value - this.scaled.y1.value;
-			console.log(this.scaled, width, height);
 			return { x: this.scaled.x1.value, y: this.scaled.y1.value, width: width, height: height };
 		}
 	},
@@ -53,24 +69,36 @@ export default
 </script>
 
 <style scoped>
-.image
+.editor
 {
 	position: relative;
+	flex: 0 0 130px;
 	height: 96px;
 	margin: 0px 5px;
 	cursor: pointer;
 	border: solid 2px #000;
+	overflow: hidden;
 }
+.editor.selected { border: solid 2px rgba(255, 0, 0, 1); }
+.editor:not(.selected):hover { border: solid 2px rgba(210, 0, 0, 1); }
 .img
 {
 	height: 100%;
 	object-fit: cover;
 	z-index: 1;
 }
+.waiting
+{
+	position: absolute;
+	width: 30px;
+	left: calc((130px - 30px) / 2);
+	top: calc((96px - 30px) / 2 - 10px);
+}
 .mask
 {
 	position:absolute;
 	left:0px; top: 0px;
+	z-index: 2;
 }
 .name
 {
@@ -81,6 +109,6 @@ export default
 	font: 11px / 20px var(--font);
 	background: rgba(0, 0, 0, .6);
 	color: #fff;
-	z-index: 5;
+	z-index: 3;
 }
 </style>
