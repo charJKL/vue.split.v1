52e65d4e367871aa47c725ca605aeb7b9686a3a9
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-18 09:21:16 +0100

    Mock `Preview` component.

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,22 +1,47 @@
 <template>
 <div :class="['preview']">
-	preview image
+	<canvas ref="canvas"></canvas>
 </div>
 </template>
 
 <script>
 import {isMatch} from '../lib/isMatch';
-
-const blueprint = 
-{
-	padding: { top: 12, right: 12, bottom: 12, left: 12 },
-}
+import {record} from '../store/records';
 
 export default
 {
 	props:
 	{
-		padding: { type: Object, default: blueprint.padding, validator(value){ return isMatch(blueprint.padding, value); } },
+		source: { type: Object, validator(value){ return isMatch(record.source, value); } },
+		metrics: { type: Object, validator(value){ return isMatch(record.metrics, value); } },
+	},
+	watch:
+	{
+		source: 'paint',
+		metrics: 'paint'
+	},
+	methods:
+	{
+		paint()
+		{
+			//var source = this.$store.getters.source;
+			var metrics = this.$store.getters.metrics;
+			//console.log('process file', value.url);
+			
+			let width = metrics.x2.value - metrics.x1.value;
+			let height = metrics.y2.value - metrics.y1.value;
+			
+			this.$refs.canvas.width = width;
+			this.$refs.canvas.height = height;
+			
+			let context = this.$refs.canvas.getContext('2d');
+			context.fillStyle = 'rgba(0, 0, 0, .3)';
+			context.fillRect(0, 0, width, height);
+			
+			//console.log(this.source);
+			
+			context.drawImage(this.source.img, metrics.x1.value, metrics.y1.value, width, height, 0, 0, width, height);
+		}
 	}
 }
 </script>
