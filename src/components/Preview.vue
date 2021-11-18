1b032e6014cbeb6622695dbaffcf1001cd374e9c
charJKL <11600464+charJKL@users.noreply.github.com>
2021-11-18 15:56:33 +0100

    Fix bug with rotation origins.

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -48,24 +48,32 @@ export default
 	{
 		paint()
 		{
-			this.temp.width = this.source.size.width;
-			this.temp.height = this.source.size.height;
-			const tempContext = this.temp.getContext("2d");
+			this.drawRotate(this.temp);
+			this.drawClip(this.temp, this.canvas);
+		},
+		drawRotate(canvas)
+		{
+			canvas.width = this.source.size.width;
+			canvas.height = this.source.size.height;
+			const context = canvas.getContext("2d");
 			
 			const halfX = this.source.size.width / 2;
 			const halfY = this.source.size.height / 2;
 			
-			tempContext.translate(halfX * -1, halfY * -1);
-			tempContext.rotate(this.metrics.rotate.value * Math.PI / 180);
-			tempContext.drawImage(this.source.img, halfX, halfY);
-			
+			context.translate(halfX, halfY);
+			context.rotate(this.metrics.rotate.value * Math.PI / 180);
+			context.translate(halfX * -1, halfY * -1);
+			context.drawImage(this.source.img, 0, 0);
+		},
+		drawClip(source, destination)
+		{
 			const width = this.metrics.x2.value - this.metrics.x1.value;
 			const height = this.metrics.y2.value - this.metrics.y1.value;
 			
-			this.canvas.width = width;
-			this.canvas.height = height;
-			const canvasContext = this.canvas.getContext("2d");
-			canvasContext.drawImage(this.temp, this.metrics.x1.value, this.metrics.y1.value, width, height, 0, 0, width, height);
+			destination.width = width;
+			destination.height = height;
+			const context = destination.getContext("2d");
+			context.drawImage(source, this.metrics.x1.value, this.metrics.y1.value, width, height, 0, 0, width, height);
 		}
 	}
 }
