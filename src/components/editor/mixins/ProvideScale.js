13ed11b6d23ad73e00c6058b7db04bd9a60f92c2
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-01 09:45:06 +0100

    Fix bug with scaled calculations.

--- src/components/editor/mixins/ProvideScale.js
+++ src/components/editor/mixins/ProvideScale.js
@@ -11,12 +11,12 @@ const ProvideScale = {
 		scaled()
 		{
 			return {
-				width: this.source.width * this.scale,
-				height: this.source.height * this.scale,
-				x1: this.metrics.x1 * this.scale,
-				x2: this.metrics.x2 * this.scale,
-				y1: this.metrics.y1 * this.scale,
-				y2: this.metrics.y2 * this.scale,
+				width: this.source.width * this.scale.x,
+				height: this.source.height * this.scale.y,
+				x1: this.metrics.x1 * this.scale.x,
+				x2: this.metrics.x2 * this.scale.x,
+				y1: this.metrics.y1 * this.scale.y,
+				y2: this.metrics.y2 * this.scale.y,
 			}
 		}
 	},
@@ -24,10 +24,8 @@ const ProvideScale = {
 	{
 		calcRatioScaleValue(viewport, size)
 		{
-			const width = viewport.width;
-			const height = viewport.height;
-			const x = width / size.width;
-			const y = height / size.height;
+			const x = viewport.width / size.width;
+			const y = viewport.height / size.height;
 			const scale = Math.min(x, y);
 			return {x: scale, y: scale};
 		}
