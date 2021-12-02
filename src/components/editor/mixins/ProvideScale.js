99c492c3c1c80c351c6d6cce5803f2bee911ce44
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-02 10:04:42 +0100

    Refactor `ProvideScale` to use native addEventListeners.

--- src/components/editor/mixins/ProvideScale.js
+++ src/components/editor/mixins/ProvideScale.js
@@ -1,9 +1,13 @@
-
+const Button = {left: 0, right: 2};
 const ProvideScale = {
 	data()
 	{
 		return {
-			scale: {x: 1.0, y: 1.0}
+			scale: {x: 1.0, y: 1.0},
+			provideScale: {
+				isScaling: false,
+				sensitivity: 0.0001,
+			}
 		}
 	},
 	computed:
@@ -28,6 +32,33 @@ const ProvideScale = {
 			const y = viewport.height / size.height;
 			const scale = Math.min(x, y);
 			return {x: scale, y: scale};
+		}
+	},
+	mounted()
+	{
+		this.$el.addEventListener('mousedown', mousedown.bind(this));
+		this.$el.addEventListener('mouseup', mouseup.bind(this));
+		this.$el.addEventListener('mouseleave', mouseleave.bind(this));
+		this.$el.addEventListener('wheel', wheel.bind(this));
+		
+		function mousedown(e)
+		{
+			if(e.button !== Button.right) return;
+			this.provideScale.isScaling = true;
+		}
+		function mouseup()
+		{
+			this.provideScale.isScaling = false;
+		}
+		function mouseleave()
+		{
+			this.provideScale.isScaling = false;
+		}
+		function wheel(e)
+		{
+			if(this.provideScale.isScaling === false) return;
+			this.scale.x = this.scale.x - e.deltaY * this.provideScale.sensitivity;
+			this.scale.y = this.scale.y - e.deltaY * this.provideScale.sensitivity;
 		}
 	}
 }
