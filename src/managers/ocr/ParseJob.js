2e43d6c69ce32b8dbd4ac847af07d76d8551231c
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-04 21:19:54 +0100

    Refactor `ParseJob` class. Use ES6 syntax.

--- src/managers/ocr/ParseJob.js
+++ src/managers/ocr/ParseJob.js
@@ -1,96 +1,136 @@
-import {Status} from '../../lib/Status';
 import {createWorker} from 'tesseract.js';
+/* eslint-disable */
+const ParseJobStatus = {Idle: 'Idle', Active: 'Active', Ended: 'Ended'};
+const ParseJobEvents = {initialize: 'initialize', recognize: 'recognize', interrupted: 'interrupted', done: 'done', ended: 'ended'};
 
-export const ParseJobStatus = {Idle: 'Idle', Active: 'Active', Completed: 'Completed', Terminated: 'Terminated'};
-
-function ParseJob(store, state, id)
+class ParseJob
 {
-	this.store = store;
-	this.state = state;
-	this.id = id;
-	this.status = ParseJobStatus.Idle;
-	this.worker = null;
-}
-ParseJob.prototype.parameters = {
-	user_defined_dpi: '96',
-	tessjs_create_hocr: '0',
-	tessjs_create_tsv: '0',
-	tessjs_create_box: '0',
-	tessjs_create_unlv: '0',
-	tessjs_create_osd: '0'
-};
-ParseJob.prototype.run = async function()
-{
-	if(this.status === ParseJobStatus.Active) return Promise.reject('You cant run active job.');
-	if(this.status === ParseJobStatus.Completed) return Promise.reject('You cant run again completed job.');
-	if(this.status === ParseJobStatus.Terminated) return Promise.reject('You cant run terminated job.');
-	
-	const cropped = this.state.records.records.get(this.id).cropped;
-	const ocr = this.state.records.records.get(this.id).ocr;
-	
-	// Create web worker:
-	this.status = ParseJobStatus.Active;
-	this.worker = createWorker({
-		workerPath: '/worker.min.js',
-		langPath: '/',
-		corePath: '/tesseract-core.wasm.js',
-		logger: this.log.bind(this),
-		errorHandler: this.terminate.bind(this),
-	});
-	
-	// Update ocr stage status:
-	ocr.status = Status.Working;
-	this.store.commit('ocr', {id: this.id, value: {...ocr}});
-	
-	// Prepare tesseract web worker:
-	await this.worker.load();
-	await this.worker.loadLanguage('eng+pol');
-	await this.worker.initialize('pol');
-	await this.worker.setParameters(this.parameters);
-	
-	// Execute tesseract recognition:
-	cropped.blob.name = 'some fake name'; // there is a bug in tesseract which require that blob should have name property.
-	const result = await this.worker.recognize(cropped.blob);
-	
-	// Update ocr stage status and save results:
-	ocr.status = Status.Completed;
-	ocr.lines = [...result.data.lines];
-	this.store.commit('ocr', {id: this.id, value: {...ocr}});
-	
-	// Remove worker:
-	this.status = ParseJobStatus.Completed;
-	await this.worker.terminate();
-	return Promise.resolve(true);
-}
-ParseJob.prototype.log = function()
-{
-	const ocr = this.state.records.records.get(this.id).ocr;
-	ocr.status = Status.Working;
-	this.store.commit('ocr', {id: this.id, value: {...ocr}});
-}
-ParseJob.prototype.terminate = function()
-{
-	if(this.status === ParseJobStatus.Active)
-	{
-		this.status = ParseJobStatus.Terminated;
-		this.worker.terminate();
+	#blob = null;
+	#status = ParseJobStatus.Idle;
+	#interrupted = false;
+	#worker = null;
+	#listeners = {
+		[ParseJobEvents.initialize]: [],
+		[ParseJobEvents.recognize]: [],
+		[ParseJobEvents.interrupted]: [],
+		[ParseJobEvents.done]: [],
+		[ParseJobEvents.ended]: []
 	}
 	
-	const ocr = this.state.records.records.get(this.id).ocr;
-	ocr.status = Status.Dirty;
-	this.store.commit('ocr', {id: this.id, value: {...ocr}});
-}
-ParseJob.prototype.isIdle = function()
-{
-	return this.status == ParseJobStatus.Idle;
-}
-ParseJob.prototype.isActive = function()
-{
-	return this.status == ParseJobStatus.Active;
-}
-ParseJob.prototype.isTerminated = function()
-{
-	return this.status == ParseJobStatus.Terminated;
+	static #parameters = {
+		user_defined_dpi: '96',
+		tessjs_create_hocr: '0',
+		tessjs_create_tsv: '0',
+		tessjs_create_box: '0',
+		tessjs_create_unlv: '0',
+		tessjs_create_osd: '0'
+	};
+
+	constructor(blob)
+	{
+		if(!(blob instanceof Blob)) throw new Error(`ParseJob take blob as data.`);
+
+		this.#blob = blob;
+		this.#blob.name = 'some-string'; // there is a bug in tesseract which require that blob should have name property.
+	}
+	
+	addEventListener(type, listener)
+	{
+		if(Object.prototype.hasOwnProperty.call(this.#listeners, type) === false) throw new Error(`ParseJob doesnt support '${type}' event.`, ParseJob);
+		this.#listeners[type].push(listener);
+	}
+	
+	dispatchEvent(type, data)
+	{
+		if(Object.prototype.hasOwnProperty.call(this.#listeners, type) === false) throw new Error(`You can't dispatch '${type}' event on ParseJob.`, ParseJob);
+		for(const listener of this.#listeners[type])
+		{
+			listener.call(this, data);
+		}
+	}
+	
+	async run()
+	{
+		if(this.#status === ParseJobStatus.Active) throw new Error(`This ParseJob is active. You can't run it again.`, this);
+		if(this.#status === ParseJobStatus.Ended) throw new Error(`This ParseJob ended. You can't run it again.`, this);
+		
+		this.#status = ParseJobStatus.Active;
+		this.#worker = createWorker({
+			workerPath: '/worker.min.js',
+			langPath: '/',
+			corePath: '/tesseract-core.wasm.js',
+			logger: this.#log.bind(this),
+			errorHandler: this.#handler.bind(this),
+		});
+		
+		await this.#worker.load();
+		await this.#worker.loadLanguage('eng+pol');
+		await this.#worker.initialize('pol');
+		await this.#worker.setParameters(ParseJob.#parameters);
+		const result = await this.#worker.recognize(this.#blob);
+		this.dispatchEvent(ParseJobEvents.done, result);
+		this.#status = ParseJobStatus.Ended;
+		this.dispatchEvent(ParseJobEvents.ended);
+		await this.#worker.terminate();
+	}
+	
+	async terminate(cause)
+	{
+		this.#worker?.terminate();
+		this.#interrupted = true;
+		this.dispatchEvent(ParseJobEvents.interrupted, cause);
+		this.#status = ParseJobStatus.Ended;
+		this.dispatchEvent(ParseJobEvents.ended);
+	}
+	
+	isIdle()
+	{
+		return this.#status === ParseJobStatus.Idle;
+	}
+	
+	isActive()
+	{
+		return this.#status === ParseJobStatus.Active;
+	}
+	
+	isEnded()
+	{
+		return this.#status === ParseJobStatus.Ended;
+	}
+	
+	wasInterrupted()
+	{
+		return this.#interrupted;
+	}
+	
+	#log(log)
+	{
+		switch(String(log.status))
+		{
+			case "loading tesseract core":
+			case "initializing tesseract":
+			case "initialized tesseract":
+			case "loading language traineddata":
+			case "loaded language traineddata":
+			case "initializing api":
+			case "initialized api":
+				this.dispatchEvent(ParseJobEvents.initialize, log);
+				return;
+			
+			case "recognizing text":
+				this.dispatchEvent(ParseJobEvents.recognize, log);
+				return;
+			
+			default:
+				console.warn('Uncatch tesseract log:', log);
+				return;
+		}
+	}
+	
+	#handler(cause) // eslint-disable-line no-dupe-class-members
+	{
+		console.log('ParseJob handler', cause);
+	}
 }
 
 export default ParseJob;
