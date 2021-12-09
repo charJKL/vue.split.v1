df7eb2ec576b8bd883c357c37ee7dca876c8eac2
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-09 10:43:18 +0100

    Refactor `Status` utils. Generate computed dynamically.

--- src/components/editor/mixins/RequireCropped.js
+++ src/components/editor/mixins/RequireCropped.js
@@ -1,6 +1,6 @@
 import {record} from '../../../store/records';
+import {mapStatusEnum} from '../../../lib/Status';
 import {isMatch} from '../../../lib/isMatch';
-import {Status} from '../../../lib/Status';
 
 const RequireCropped = {
 	props:
@@ -9,46 +9,10 @@ const RequireCropped = {
 	},
 	computed:
 	{
-		
+		...mapStatusEnum('cropped'),
 		getCroppedUrl()
 		{
 			return this.cropped?.blob ? URL.createObjectURL(this.cropped.blob) : null;
-		},
-		printCroppedStatus()
-		{
-			return Status.toString(this.cropped.status);
-		},
-		isCroppedNotNull()
-		{
-			return this.cropped != null;
-		},
-		isCroppedNull()
-		{
-			return this.cropped == null;
-		},
-		isCroppedDirty()
-		{
-			return this.cropped.status === Status.Dirty;
-		},
-		isCroppedWaiting()
-		{
-			return this.cropped.status === Status.Waiting;
-		},
-		isCroppedQueued()
-		{
-			return this.cropped.status === Status.Queued;
-		},
-		isCroppedWorking()
-		{
-			return this.cropped.status === Status.Working;
-		},
-		isCroppedCompleted()
-		{
-			return this.cropped.status === Status.Completed;
-		},
-		isCroppedNotCompleted()
-		{
-			return this.cropped.status !== Status.Completed;
 		}
 	},
 	watch:
--- src/lib/Status.js
+++ src/lib/Status.js
@@ -1,7 +1,23 @@
 
 const Status = { Dirty: 1, Waiting: 2, Queued: 3, Loading: 4, Working: 5, Error: 6, Completed: 7 }
 
-Status.toString = function(status)
+function mapStatusEnum(name)
+{
+	var properties = {};
+	const uppercaseName = name.charAt(0).toUpperCase() + name.slice(1);
+	for(const [key, value] of Object.entries(Status))
+	{
+		properties[`is${uppercaseName}${key}`] = function(){ return this?.[name].status === value };
+	}
+	properties['printStatus'] = function(){ return statusToString(this?.[name].status); }
+	properties['printDetauils'] = function(){ return this?.[name].details; }
+	properties[`is${uppercaseName}NotNull`] = function(){ return this?.[name] != null; }
+	properties[`is${uppercaseName}NotCompleted`] = function(){ return this?.[name].status != Status.Completed };
+	
+	return properties;
+}
+
+function statusToString(status)
 {
 	for (let [key, value] of Object.entries(Status))
 	{
@@ -11,4 +27,4 @@ Status.toString = function(status)
 }
 
 export default Status;
-export {Status};
+export {Status, mapStatusEnum};
