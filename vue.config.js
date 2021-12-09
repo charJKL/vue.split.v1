7f9af216a15ba6a566c0d6355b1597c6a73dfb2b
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-09 12:20:14 +0100

    Export commond styles to separate scss file.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
+
 module.exports = 
 {
 	publicPath: './',
-	
+	css: {
+	loaderOptions: {
+		sass: {
+				additionalData: `@import "@/App.scss"; @import "@/components/editor/Editor.scss";`
+			}
+		}
+	}
 }
