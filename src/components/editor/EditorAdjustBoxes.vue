208f9a3b1ee98b0918b9379fb8edaf7a1ea7cde8
charJKL <11600464+charJKL@users.noreply.github.com>
2021-12-14 15:23:25 +0100

    Fix bug with disappearing word boxes on editor change.

--- src/components/editor/EditorAdjustBoxes.vue
+++ src/components/editor/EditorAdjustBoxes.vue
@@ -13,15 +13,18 @@ export default
 	{
 		boxes(value)
 		{
-			const ctx = this.$refs.canvas.getContext('2d');
-			ctx.clearRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
-			ctx.strokeStyle = "rgb(255,0,0)";
-			for(const box of value)
+			this.$nextTick(function()
 			{
-				const width = box.bbox.x1 - box.bbox.x0;
-				const height = box.bbox.y1 - box.bbox.y0;
-				ctx.strokeRect(box.bbox.x0, box.bbox.y0, width, height);
-			}
+				const ctx = this.$refs.canvas.getContext('2d');
+				ctx.clearRect(0, 0, this.$refs.canvas.width, this.$refs.canvas.height);
+				ctx.strokeStyle = "rgb(255,0,0)";
+				for(const box of value)
+				{
+					const width = box.bbox.x1 - box.bbox.x0;
+					const height = box.bbox.y1 - box.bbox.y0;
+					ctx.strokeRect(box.bbox.x0, box.bbox.y0, width, height);
+				}
+			})
 		}
 	}
 }
